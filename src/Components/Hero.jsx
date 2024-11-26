76c538edd12c42e91a1ea5f11f50a66f46a55b8d
imrankabir02 <imrankabir325@gmail.com>
2024-11-26 16:36:00 +0600

    code updated

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
             <div class="grid max-w-md grid-cols-1 mx-auto lg:grid-cols-12 gap-x-6 gap-y-8 lg:max-w-none">
                 <div class="self-center lg:col-span-4">
                     <h1 class="text-3xl font-bold text-white sm:text-4xl xl:text-5xl">Hey 👋 I am Brian Jones, writing on NFTs.</h1>
-                    <p class="mt-5 text-base font-normal leading-7 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vehicula massa in enim luctus.</p>
+                    <p class="mt-5 text-base font-normal leading-7 text-gray-100">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vehicula massa in enim luctus.</p>
                     <div class="relative inline-flex mt-9 group">
                         <div class="absolute transitiona-all duration-1000 opacity-70 inset-0 bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg filter group-hover:opacity-100 group-hover:duration-200"></div>
 
